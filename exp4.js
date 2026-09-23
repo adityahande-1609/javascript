77d6a9b663b8658527776bbffaec0fa8a26b2dd4
Aditya hande <hande.adu@gmail.com>
2026-09-24 01:19:04 +0530

    Implement form submission event listener
    Added form submission handling and logging for name and password fields.

--- exp4.js
+++ exp4.js
@@ -1,2 +1,15 @@
-const form_name =document.getElementById("name") 
-console.log(form_name)
+let form = document.getElementById("myform");
+        form.addEventListener("submit",function(event){
+        
+        event.preventDefault();
+        let form_name =document.getElementById("name").value;
+        console.log("name :",form_name);
+        let form_pass=document.getElementById("password").value;
+        console.log("pass :",form_pass);
+        // let  Reading=document.getElementById("reading").checked
+        // console.log(Reading)
+        // let traveled=document.getElementById("travel").checked
+        // console.log(traveled)
+        // let cooking=document.getElementById("cook").checked
+        // console.log(cooking.value)
+        // let gMale = document.getElementById("Male").checked
